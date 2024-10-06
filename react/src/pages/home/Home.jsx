3886a2d6f3b5034bbaea57d381e6369c4a2593ca
bhasker-sharma <bhaskersharma13@gmail.com>
2024-10-07 00:55:14 +0530

    first section of landing page was created, on the body section positioning problem is comming due to which that portion is commented

--- react/src/pages/home/Home.jsx
+++ react/src/pages/home/Home.jsx
@@ -2,18 +2,46 @@ import React from 'react';
 import './Home.css';
 import Navbar from '../../components/navbar/Navbar';
 import Footer from '../../components/footer/Footer';
-import illustration from '../../assets/img_1.png'; // Import illustration image
+import illustration1 from '../../assets/img_1.png'; // Import illustration image
+import illustration2 from '../../assets/img_2.png';
 
 const Home = () => {
   return (
-    <>
+    <div className='home'>
       <Navbar />
-      <div className="container">
-        {/* Illustration image positioned in top right */}
-        <img src={illustration} alt="Illustration" className="illustration" />
+
+      {/* Hero section */}
+      <div className="hero-section">
+        <img src={illustration1} alt="Illustration" className="illustration_1" />
+        
+        <div className='hero-content'>
+          <h1>Our strategies, your growth in Bussiness</h1>
+          <p>The Place wehre you get to know what's stopping you behind</p>
+
+          <div className='hero-button'>
+            <button className='services-btn'>SEE SERVICES</button>
+            <button className='contactus-btn'>CONTACT US</button>
+          </div>
+        </div>
       </div>
+
+      {/* Body section */}
+      {/* <div className='body-section'> */}
+
+        {/* First row */}
+        {/* <div className='row1'>
+          <div className='text-card'>
+            <h2>Brand voice</h2>
+            <p>Strengthen your brand identity with clear, consistent message that resonates with your audience. Developing the brand that speaks. </p>
+          </div>
+
+        </div> */}
+
+      {/* </div> */}
+
+
       <Footer />
-    </>
+    </div>
   );
 };
 
