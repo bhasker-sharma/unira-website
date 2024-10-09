6aa2b4404868cd7f42ac8060d574c8d54482dcb9
Shivam Jain <shivam03jain@gmail.com>
2024-10-10 00:32:48 +0530

    contact us

--- react/src/components/navbar/Navbar.jsx
+++ react/src/components/navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => { //function is made
           <li><Link to="/">Home</Link></li>
           <li><Link to="/services">Services</Link></li>
           <li><Link to="/about">About Us</Link></li>
-          <li><Link to="/contact">Contact Us</Link></li>
+          <li><Link to="/contactus">Contact Us</Link></li>
         </ul>
       </div>
       </nav>
