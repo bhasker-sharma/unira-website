f6599fda6e13a10e31f72b5584deecce948bb059
bhasker-sharma <bhaskersharma13@gmail.com>
2024-10-09 22:08:52 +0530

    here contact us page is added

--- react/src/App.js
+++ react/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
+import Contactus from './pages/contactus/Contactus';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/contactus" element={<Contactus/>} />
         </Routes>
       </main>
     </Router>
---
+++ react/src/pages/contactus/Contactus.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Footer from "../../components/footer/Footer";
+import Navbar from "../../components/navbar/Navbar";
+import "./Contactus.css";
+
+
+const Contactus = () => {
+  return (
+    <div className="home">
+        <Navbar />
+    <div className="Bodysection">
+        Hi herre the form will come
+    </div>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Contactus;
